23604454d6a8f94aa1f84e9dbf12e5f7a4258731
Scott Crunkleton <crunk1@gmail.com>
2013-11-27 13:21:38 -0700

    Kalman updating stuff put in.

--- bzagents/hunter.py
+++ bzagents/hunter.py
@@ -2,9 +2,10 @@ __author__ = 'crunk'
 
 import sys
 from bzrc import *
-from numpy import *
+from numpy import matrix
 
 ###########################Kalman Functions#############################
+
 
 ########################################################################
 # 
@@ -13,12 +14,14 @@ def mu_plus_one():
 
     return
 
+
 ########################################################################
 # 
 ########################################################################
 def sigma_plus_one():
 
     return
+
 
 ########################################################################
 # 
@@ -31,7 +34,7 @@ def k_plus_one():
 #----------------------------------------------------------------
 
 ###########################Constants And Stuff#############################
-mu_not    = matrix('000000')
+mu_not = matrix('000000')
 
 sigma_not = matrix('100  0   0    0   0   0  ;\
                       0  0.1 0    0   0   0  ;\
@@ -40,32 +43,38 @@ sigma_not = matrix('100  0   0    0   0   0  ;\
                       0  0   0    0   0.1 0  ;\
                       0  0   0    0   0   0.1')
 
+I = matrix('1 0 0 0 0 0;'
+           '0 1 0 0 0 0;'
+           '0 0 1 0 0 0;'
+           '0 0 0 1 0 0;'
+           '0 0 0 0 1 0;'
+           '0 0 0 0 0 1')
+
 H = matrix('1 0 0 0 0 0;\
             0 0 0 1 0 0')
 
 sigma_z = matrix('25  0;\
                    0 25')
 
+
 def F(delta_t=0.5):
     c = -0.1
     # c = 0.0
 
-    m =     '1 ' + delta_t + ' ' + (pow(delta_t,2)/2.0) + ' 0 0 0;'
-    m = m + '0 1 ' + delta_t + ' 0 0 0;'
-    m = m + '0 ' + c + ' 1 0 0 0;'
-    m = m + '0 0 0 1 ' + delta_t + ' ' + (pow(delta_t,2)/2.0) + ';'
-    m = m + '0 0 0 0 1 ' + delta_t + ';'
-    m = m + '0 0 0 0 ' + c + ' 1'   
+    m =     '1 ' + str(delta_t) + ' ' + str(pow(delta_t, 2)/2.0) + ' 0 0 0;'
+    m = m + '0 1 ' + str(delta_t) + ' 0 0 0;'
+    m = m + '0 ' + str(c) + ' 1 0 0 0;'
+    m = m + '0 0 0 1 ' + str(delta_t) + ' ' + str(pow(delta_t, 2)/2.0) + ';'
+    m = m + '0 0 0 0 1 ' + str(delta_t) + ';'
+    m = m + '0 0 0 0 ' + str(c) + ' 1'
     return matrix(m)
 
-sigma_x =   matrix('  0.1 0   0    0   0   0  ;\
-                      0   0.1 0    0   0   0  ;\
-                      0   0   100  0   0   0  ;\
-                      0   0   0    0.1 0   0  ;\
-                      0   0   0    0   0.1 0  ;\
-                      0   0   0    0   0   100')
-
-
+sigma_x = matrix('0.1 0   0    0   0   0  ;\
+                  0   0.1 0    0   0   0  ;\
+                  0   0   100  0   0   0  ;\
+                  0   0   0    0.1 0   0  ;\
+                  0   0   0    0   0.1 0  ;\
+                  0   0   0    0   0   100')
 
 #-----------------------------END--------------------------------
 #----------------------------------------------------------------
@@ -73,21 +82,39 @@ sigma_x =   matrix('  0.1 0   0    0   0   0  ;\
 
 class Agent(object):
     def __init__(self, bzrc):
+        global mu_not, sigma_not
         self.bzrc = bzrc
         self.constants = self.bzrc.get_constants()
         self.commands = []
         self.tanks = []
-        self.ducks = []
+        self.hunter = None
         self.enemies = []
         self.target = None
+        self.kalman_vars = {'mu': mu_not, 'sigma': sigma_not}
 
     def tick(self, time_diff):
         my_tanks, other_tanks, flags, shots = self.bzrc.get_lots_o_stuff()
         self.tanks = my_tanks
-        self.ducks = other_tanks
+        if self.hunter is None:
+            self.hunter = self.tanks[0]
         self.enemies = [tank for tank in other_tanks if tank.color !=
                         self.constants['team']]
-        self.target = self.enemies[0]
+        if len(self.enemies) > 0:
+            self.target = self.enemies[0]
+        else:
+            self.target = None
+
+        self.kalman_update()
+
+    def kalman_update(self):
+        global H, I, sigma_x, sigma_z
+        tmp = F()*self.kalman_vars['sigma']*F().T + sigma_x
+        k = tmp*H.T*(H*tmp*H.T + sigma_z).I
+        z = matrix([[self.target.x], [self.target.y]])
+        new_mu = F()*self.kalman_vars['mu'] + k*(z - H*F()*self.kalman_vars['mu'])
+        new_sigma = (I - k*H)*tmp
+        self.kalman_vars['mu'] = new_mu
+        self.kalman_vars['sigma'] = new_sigma
 
 
 def main():
